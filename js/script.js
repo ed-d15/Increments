44ebb848ae097ce2a29f62737a975e169d9339a6
Jamie Hammill <jamiewlh2@gmail.com>
2026-02-15 17:57:37 -0500

    added basic clicker functionality and multiplier upgrade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
-window.addEventListener('load', function () {
+let click_count = 0;
+let pointsPerClick = 1;
 
+multiplierLevel = 0;
+const multiplierUpgrades = [
+    { cost: 20, newPPC: 2 },
+    { cost: 100, newPPC: 5 },
+    { cost: 500, newPPC: 10 },
+    { cost: 2000, newPPC: 15 },
+    { cost: 10000, newPPC: 20 }
+];
+
+window.addEventListener('load', function () {
+    const clicker = document.getElementById('clicker');
+    const scoreboard = document.getElementById('scoreboard');
+    const multiplier = document.getElementById('multiplier');
+
+    function addPoint() {
+        click_count += pointsPerClick;
+        scoreboard.innerHTML = click_count;
+    }
+
+    function multiplierUpgrade() {
+        if (click_count >= multiplierUpgrades[multiplierLevel].cost) {
+            pointsPerClick = multiplierUpgrades[multiplierLevel].newPPC;
+            click_count -= multiplierUpgrades[multiplierLevel].cost;
+            multiplierLevel++;
+            scoreboard.innerHTML = click_count;
+        }
+    }
+
+    clicker.addEventListener('click', () => addPoint());
+    multiplier.addEventListener('click', () => multiplierUpgrade())
 });
