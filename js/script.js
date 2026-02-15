a238753c5c96561619e5d52fadcc4b3f31660280
Jamie Hammill <jamiewlh2@gmail.com>
2026-02-15 18:46:06 -0500

    added autoclicker upgrade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let click_count = 0;
 let pointsPerClick = 1;
 
-multiplierLevel = 0;
-const multiplierUpgrades = [
+let manualMultiplierLevel = 0;
+const manualMultiplierUpgrades = [
     { cost: 20, newPPC: 2 },
     { cost: 100, newPPC: 5 },
     { cost: 500, newPPC: 10 },
@@ -10,25 +10,63 @@ const multiplierUpgrades = [
     { cost: 10000, newPPC: 20 }
 ];
 
+let autoClicker;
+let autoClickerOn = false;
+let automaticMultiplierLevel = 0;
+const automaticMultiplierUpgrades = [
+    { cost: 100, newPPC: 1 },
+    { cost: 500, newPPC: 2 },
+    { cost: 1500, newPPC: 3 },
+    { cost: 2500, newPPC: 5 },
+    { cost: 5000, newPPC: 8 },
+    { cost: 10000, newPPC: 10 }
+]
+
 window.addEventListener('load', function () {
     const clicker = document.getElementById('clicker');
     const scoreboard = document.getElementById('scoreboard');
-    const multiplier = document.getElementById('multiplier');
+    const manualMultiplier = document.getElementById('multiplier');
+    const automaticMultiplier = document.getElementById('automaticClickerMultiplier');
 
-    function addPoint() {
-        click_count += pointsPerClick;
+    function addPoint(points) {
+        click_count += points;
         scoreboard.innerHTML = click_count;
     }
 
-    function multiplierUpgrade() {
-        if (click_count >= multiplierUpgrades[multiplierLevel].cost) {
-            pointsPerClick = multiplierUpgrades[multiplierLevel].newPPC;
-            click_count -= multiplierUpgrades[multiplierLevel].cost;
-            multiplierLevel++;
+    function manualMultiplierUpgrade(cost, newPPC) {
+        if (click_count >= cost) {
+            pointsPerClick = newPPC;
+            click_count -= cost;
+            manualMultiplierLevel++;
             scoreboard.innerHTML = click_count;
         }
     }
 
-    clicker.addEventListener('click', () => addPoint());
-    multiplier.addEventListener('click', () => multiplierUpgrade())
+    function addAutomaticClicker(newPPC) {
+        autoClicker = setInterval(() => addPoint(newPPC), 1000);
+        autoClickerOn = true;
+    }
+
+    function stopAutomaticClicker() {
+        clearInterval(autoClicker);
+        autoClickerOn = false;
+    }
+
+    function automaticMultiplierUpgrade(cost, newPPC) {
+        if (click_count >= cost) {
+            click_count -= cost;
+            scoreboard.innerHTML = click_count;
+
+            if (autoClickerOn == true) {
+                stopAutomaticClicker();
+            }
+            addAutomaticClicker(newPPC);
+
+            automaticMultiplierLevel++;
+        }
+    }
+
+    clicker.addEventListener('click', () => addPoint(pointsPerClick));
+    manualMultiplier.addEventListener('click', () => manualMultiplierUpgrade(manualMultiplierUpgrades[manualMultiplierLevel].cost, manualMultiplierUpgrades[manualMultiplierLevel].newPPC));
+    automaticMultiplier.addEventListener('click', () => automaticMultiplierUpgrade(automaticMultiplierUpgrades[automaticMultiplierLevel].cost, automaticMultiplierUpgrades[automaticMultiplierLevel].newPPC))
 });
